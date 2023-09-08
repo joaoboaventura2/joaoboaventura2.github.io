a0446ab38a097a5edc8afe9d282419806cc5bee7
joaoboaventura2 <144469101+joaoboaventura2@users.noreply.github.com>
2023-09-08 13:39:31 -0400

    Add files via upload

--- connection.php
+++ connection.php
@@ -13,4 +13,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Retrieve data from the form
+    $team_name = $_POST["team_name"];
+    $stadium_name = $_POST["stadium_name"];
+
+    // Insert data into the database
+    $sql = "INSERT INTO your_table_name (name, email) VALUES ('$name', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the database connection
+    $conn->close();
+}
 ?>
